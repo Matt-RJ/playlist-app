cfcf46c5518ad2ff2e1b8a35b8ab4bbeaf019d28
Mantas Rajackas <MntsRJ@gmail.com>
2019-10-30 17:56:30 +0000

    Fixed Bootstrap not importing properly and created a static Header component

--- playlist-app/src/components/header/index.js
+++ playlist-app/src/components/header/index.js
@@ -5,7 +5,20 @@ class Header extends Component {
 
 	render() {
 		return (
-			<h1>TODO - Header</h1>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-md-8 offset-4">
+						<div className="header">
+							<h1>
+								Total playlists: 
+								<span className="badge badge-pill badge-info">{this.props.noPlaylists}</span>
+								Total songs:
+								<span className="badge badge-pill badge-info">{this.props.noSongs}</span>
+							</h1>
+						</div>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
--- playlist-app/stories/index.js
+++ playlist-app/stories/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-//import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import Header from '../src/components/header/'
 
